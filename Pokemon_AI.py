9529b43acd86a147ccb1fa7c042404a521bfdfa7
Gordon Fountain <Gordonthecellist@gmail.com>
2022-05-05 18:19:42 -0400

    dd pixel detection to Pokemon Script. First semblance of intelligence

--- Pokemon_AI.py
+++ Pokemon_AI.py
@@ -1,11 +1,162 @@
+from PIL import ImageGrab
 import keyboard
 import random
 import time
 import sys
 
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+BATTLE_MENU_X = 1680
+BATTLE_MENU_Y = 733
+BATTLE_ACTIVE = (248, 248, 248)
+
+HEALTH_X = 1290
+HEALTH_Y = 613
+
 used_keys = [
     'w', 'a', 's', 'd', 'x', 'z', 'enter'
 ]
+
+def get_img():
+    img = ImageGrab.grab(bbox=None, include_layered_windows=False, \
+                             all_screens=False, xdisplay=None)
+    px = img.load()
+    return px
+
+def check_state():
+    img = get_img()
+    state = "walk"
+    if img[BATTLE_MENU_X, BATTLE_MENU_Y] == BATTLE_ACTIVE:
+        health_px = img[HEALTH_X, HEALTH_Y]
+        if health_px[1] > 200:
+            state = "battle"
+        else:
+            state = "run"
+                
+    return state
+
+def battle():
+    # Exit out of any menu game is currently in
+    keyboard.press('x')
+    time.sleep(0.2)
+    release_all()
+
+    # Move to option
+    keyboard.press('w')
+    time.sleep(0.2)
+    keyboard.press('a')
+    time.sleep(0.2)
+    release_all()
+
+    keyboard.press('z')
+    time.sleep(0.2)
+    release_all()
+    
+    move = random.randint(1, 4)
+    if move == 1:
+        keyboard.press('w')
+        time.sleep(0.2)
+        keyboard.press('a')
+
+    elif move == 2:
+        keyboard.press('w')
+        time.sleep(0.2)
+        keyboard.press('d')
+        
+    elif move == 3:
+        keyboard.press('s')
+        time.sleep(0.2)
+        keyboard.press('a')
+
+    else:
+        keyboard.press('s')
+        time.sleep(0.2)
+        keyboard.press('d')
+
+    time.sleep(0.2)
+    release_all()
+
+    keyboard.press('z')
+    time.sleep(0.2)
+    release_all()
+
+def switch_pokemon():
+    # Exit out of any menu game is currently in
+    keyboard.press('x')
+    time.sleep(0.2)
+    release_all()
+
+    # Move to option
+    keyboard.press('s')
+    time.sleep(0.2)
+    keyboard.press('a')
+    time.sleep(0.2)
+    release_all()
+
+    keyboard.press('z')
+    time.sleep(0.2)
+    release_all()
+    
+    pokemon = random.randint(0, 5)
+    for i in range(pokemon):
+        keyboard.press('s')
+        time.sleep(0.2)
+        release_all()
+        
+    time.sleep(0.2)
+    
+    keyboard.press('z')
+    time.sleep(0.2)
+    release_all()
+
+    time.sleep(0.2)
+
+    keyboard.press('z')
+    time.sleep(0.2)
+    release_all()
+
+def run():
+    # Exit out of any menu game is currently in
+    keyboard.press('x')
+    time.sleep(0.2)
+    release_all()
+
+    coin = random.randint(0, 9)
+
+    if coin > 0:
+        switch_pokemon()
+    else:
+        # Escape
+        # Move to option
+        keyboard.press('s')
+        time.sleep(0.2)
+        keyboard.press('d')
+        time.sleep(0.2)
+        release_all()
+
+        keyboard.press('z')
+        time.sleep(0.2)
+        release_all()
+
+def walk():
+    randInt = random.randint(0, 360)
+    if(randInt < 100):
+        keyboard.press('z')
+    elif randInt < 150 :
+        keyboard.press('x')
+    elif randInt < 200 :
+        keyboard.press('w')
+    elif randInt < 250 :
+        keyboard.press('a')
+    elif randInt < 300 :
+        keyboard.press('s')
+    elif randInt < 350 :
+        keyboard.press('d')
+    else :
+        keyboard.press('enter')
+    time.sleep(0.2)
+    release_all()
 
 def release_all():
     for key in used_keys:
@@ -17,27 +168,17 @@ def main():
 
     while (True):
         if keyboard.is_pressed('Esc'):
-                print("Exiting...")
-                release_all()
-                sys.exit(0)
-
-        randInt = random.randint(0, 360)
-        if(randInt < 100):
-            keyboard.press('z')
-        elif randInt < 150 :
-            keyboard.press('x')
-        elif randInt < 200 :
-            keyboard.press('w')
-        elif randInt < 250 :
-            keyboard.press('a')
-        elif randInt < 300 :
-            keyboard.press('s')
-        elif randInt < 350 :
-            keyboard.press('d')
-        else :
-            keyboard.press('enter')
-        time.sleep(0.2)
-        release_all()
+            print("Exiting...")
+            release_all()
+            sys.exit(0)
+        else:
+            state = check_state()
+            if state == "battle":
+                battle()
+            elif state == "run":
+                run()
+            else:
+                walk()
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # python-random-game-controller
 Fun scrips that "play" games autonomously. Meant for humor.
 
+# Dependancies
+Pillow library
+Installation:
+$ pip install Pillow
+
 ## Exiting Execution
 Holding the "Esc" key will always exit code execution.
 
@@ -17,13 +22,13 @@ Chaos incarnate and a lot of unknown commands.
 This script may cause the game to close (if the character dies for example), so to be safe, having a text editing software open behind the game will prevent it typing cryptic messages to your loved ones.
 
 ## Pokemon_AI
-This script runs on a pokemon emulator, the keys that are used are seen in the script and can be changed easily.
+This script runs on a pokemon emulator, the keys that are used are seen in the script and can be changed easily. Now can detect if in a battle and if health is green or not. Behaves based on this.
 
 ### Achievements
-This script has managed to catch a shiny pidgey and after 9 hours, defeat the first gym leader.
+This script has managed to catch a shiny pidgey and after 9 hours, defeat the first gym leader, and run in circles for another 2 hours.
 
 ### Behaviour
-This script enjoys opening and closing the bag, and running away from nearly fainted opponents.
+This script enjoys opening and closing the bag, and previously running away from nearly fainted opponents. Now only runs away 1/10 the time, and 9/10 the time does a sloppy job switching pokemon.
 
 ## Spelunky_AI
 Script to play Spelunky.
